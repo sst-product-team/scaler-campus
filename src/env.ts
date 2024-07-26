9930e6b98500e2a9560e6b58efd0a54d1d122a71
skushagra <kushagra.rigel@gmail.com>
2024-07-27 02:42:32 +0530

    add enviornment variables

---
+++ src/env.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class Env {
+    public static readonly port = Number(process.env.PORT) || 2404; // default port to listen
+    public static readonly dbUrl = process.env.DB_URL ?? ''; // database url
+    public static readonly ENCRYPT_SALT = Number(process.env.ENCRYPT_SALT) || 10; // number of salt rounds for hashing   
+    public static readonly AUTH_SENDER_EMAIL = process.env.AUTH_SENDER_EMAIL ?? ''; // email address of the sender
+    public static readonly AUTH_SENDER_PASSWORD = process.env.AUTH_SENDER_PASSWORD ?? ''; // password of the sender
+    public static readonly PASSWORD_CHARSET = process.env.PASSWORD_CHARSET ?? 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?/'; // charset for password generation
+    public static readonly JWT_SECRET = process.env.JWT_SECRET ?? 'scaler-secret-key-123'; // secret key for jwt
+    public static readonly JWT_EXPIRY = process.env.JWT_EXPIRE ?? '3d'; // expiry time for jwt
+}
+
+export default Env;
